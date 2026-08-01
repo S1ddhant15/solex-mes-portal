ac0b31459944a1d87c93e8725993cf6ce5d45609
S1ddhant15 <siddhant.t@solex.in>
2026-08-01 23:39:32 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,52 +1,83 @@
-// ============================
-// Solex MES Dashboard
-// ============================
+// ===================================
+// Solex MES Dashboard Authentication
+// ===================================
 
-// Check Login
 const department = localStorage.getItem("department");
+
+
+// If no login, return to login page
 
 if (!department) {
     window.location.href = "index.html";
 }
 
-// Show Department
-document.getElementById("dept").innerHTML = "👤 " + department + " Department";
+
+// Display department
+
+document.getElementById("dept").innerHTML =
+    "👤 " + department + " Department";
+
 
 // Logout
-function logout() {
+
+function logout(){
+
     localStorage.removeItem("department");
+
     window.location.href = "index.html";
+
 }
 
-// Department-wise Report URLs
-const reports = {
 
-    Production:
-        "https://app.powerbi.com/reportEmbed?reportId=PRODUCTION_REPORT_ID",
+// ===================================
+// Power BI Page Configuration
+// ===================================
 
-    Quality:
-        "https://app.powerbi.com/reportEmbed?reportId=QUALITY_REPORT_ID",
+const reportId = 
+"b6ac0c8d-cad0-4b72-beaa-fc4dc3f0d9e4";
 
-    Maintenance:
-        "https://app.powerbi.com/reportEmbed?reportId=MAINTENANCE_REPORT_ID",
+
+const tenantId =
+"efe10ad5-9f60-494c-991c-f4b5a28390ba";
+
+
+// Department wise pages
+
+const pages = {
 
     Management:
-        "https://app.powerbi.com/reportEmbed?reportId=MANAGEMENT_REPORT_ID"
+    "7b89b3c91e4d95e740e7",
+
+    Production:
+    "d583f0c64dd166a90eaa",
+
+    Quality:
+    "7b89b3c91e4d95e740e7",
+
+    Process:
+    "a86e5f3003a48dcee473",
+
+    Maintenance:
+    "994fe46fc27806d8e5c4"
 
 };
 
-// Temporary: same report for all departments
-reports.Production =
-"https://app.powerbi.com/reportEmbed?reportId=b6ac0c8d-cad0-4b72-beaa-fc4dc3f0d9e4&autoAuth=true&ctid=efe10ad5-9f60-494c-991c-f4b5a28390ba";
 
-reports.Quality =
-reports.Production;
+// Select page
 
-reports.Maintenance =
-reports.Production;
+let pageName = pages[department];
 
-reports.Management =
-reports.Production;
 
-// Load Report
-document.getElementById("powerbiFrame").src = reports[department];
+// Power BI Embed URL
+
+let powerBIURL =
+"https://app.powerbi.com/reportEmbed?" +
+"reportId=" + reportId +
+"&pageName=" + pageName +
+"&autoAuth=true" +
+"&ctid=" + tenantId;
+
+
+// Load Power BI
+
+document.getElementById("powerbiFrame").src = powerBIURL;
